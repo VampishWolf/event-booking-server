42017808e0b00862f1e84f42af7024e1b97caf5a
Ashish Mittal <extern.mittal_ashish@allianz.com>
2021-02-28 23:23:12 +0530

    heroku integration

--- server/config/events.config.js
+++ server/config/events.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     port: 5000,
-    clienturl: 'localhost:3000',
+    clienturl: 'blue-mountains.netlify.app',
     // dburl: 'mongodb://localhost:27017/eventbuy'
     dburl: 'mongodb+srv://ashish:k!ng0258@cluster0.eawc5.mongodb.net/eventbuy?retryWrites=true&w=majority'
 }
